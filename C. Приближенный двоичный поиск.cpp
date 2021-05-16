594bd1990e6be536b8f89a81a951eb1058e18b58
AlKharkov <79756667+AlKharkov@users.noreply.github.com>
2021-05-16 18:56:09 +0700

    Update C. Приближенный двоичный поиск.cpp

--- C. Приближенный двоичный поиск.cpp
+++ C. Приближенный двоичный поиск.cpp
@@ -1,32 +1,32 @@
 #include <iostream>
 #include <vector>
-#include <string>
 #include <cmath>
+#include <algorithm>
 
 using namespace std;
 
 int main() {
-    int n, k, b;
+    int n, k, v;
     cin >> n >> k;
     vector <int> a;
-    for (int i = 0; i < n; ++i) {  // считываем сразу так, чтобы не было повторяющихся элементов
-        cin >> b;
-        if (i != 0) {
-            if (a[a.size() - 1] != b) a.push_back(b);
-        } else a.push_back(b);
+    vector <pair<int, int>> b(k);  // значение, индекс вхождения
+    for (int i = 0; i < n; ++i) {
+        cin >> v;
+        if (i == 0) a.push_back(v);
+        else if (a[a.size() - 1] != v) a.push_back(v);
     }
     for (int i = 0; i < k; ++i) {
-        int l = -1, r = a.size();  // рассматриваем отрезок от l до r(не включая концы)
-        cin >> b;
-        while (r - l > 2) {
-            int mark = (l + r) / 2;
-            if (l != mark - 1 && mark != 0) {  // при l = -1 r = 2 => mark = 0
-                if (abs(b - a[mark - 1]) > abs(b - a[mark])) l = mark - 1;
-                else r = mark;
-            } else {  // if (r != mark + 1)
-                if (abs(b - a[mark]) > abs(b - a[mark + 1])) l = mark;
-                else r = mark + 1;
-            }
-        } cout << a[(l + r) / 2] << '\n';
+        cin >> v;
+        b[i] = {v, i};
+    } sort(b.begin(), b.end());
+    vector <int> ans(k, -1);
+    long ma = 0;
+    for (int mb = 0; mb < k; ++mb) {
+        while (ans[b[mb].second] == -1) {
+            if ((int) a.size() == ma + 1) ans[b[mb].second] = a[ma];
+            else if (abs(b[mb].first - a[ma]) <= abs(b[mb].first - a[ma + 1])) ans[b[mb].second] = a[ma];
+            else ++ma;
+        }
     }
+    for (int i: ans) cout << i << '\n';
 }
